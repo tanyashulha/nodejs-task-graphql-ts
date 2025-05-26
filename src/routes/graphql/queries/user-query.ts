62159a9ebe409731b6bf4d1caa6d91c510752be2
tatsiana_shulha <t.shulha@symfa.com>
2025-05-26 07:30:00 +0200

    small fix

--- src/routes/graphql/queries/user-query.ts
+++ src/routes/graphql/queries/user-query.ts
@@ -28,7 +28,6 @@ export const UserQueryFields = {
             }});
 
             return users.forEach((user) => ctx.user.prime(user.id, user));
-            return users.forEach((user) => ctx.user.prime(user?.id, user));
         },
     },
 }
